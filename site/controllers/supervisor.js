f485d82b04514d34985a1eceda4a1e155c28ac83
Leaderfirestar <EricAWebb2000@yahoo.com>
2020-03-12 22:46:32 -0500

    Added a edit user function. Do we need to add a password reset function?

--- site/controllers/supervisor.js
+++ site/controllers/supervisor.js
@@ -5,45 +5,71 @@ let bcrypt = require('bcrypt');
 let uniqid = require('uniqid');//This and bcrypt did NOT show up on their own in the package json. Best of luck
 
 exports.get_clinicians = function(req, res, next){
-    res.render('clinicians/clinicians');
+  res.render('clinicians/clinicians');
 }
 
 exports.create_user[
-    body('username').isLength({min: 1}).trim().withMessage('Please enter your username').isAlphaNumeric().withMessage('Must be alphanumeric'),
-    body('password').isLength({min: 1}).trim().withMessage('Please enter your password'),
-    body('fname').isLength({min: 1}).trim().withMessage('Please enter your first name').isAlphaNumeric().withMessage('Must be alphanumeric'),
-    body('lname').isLength({min: 1}).trim().withMessage('Please enter your last name').isAlphaNumeric().withMessage('Must be alphanumeric'),
-    body('phone').isLength({min: 12, max: 12}).trim().withMessage('Please enter your phone number (eg. 555-123-4567)'),
-    body('email').isLength({min: 1}).trim().withMessage('Please enter your email (eg. example@domain.com)'),
-    //sanitize
-    sanitize('username').escape(),
-    sanitize('password').escape(),
-    sanitize('fname').escape(),
-    sanitize('lname').escape(),
-    sanitize('phone').escape(),
-    sanitize('email').escape(),
-    (req, res, next) =>{
-      const errors = validationResult(req);
-      if (!errors.isEmpty()){
-        //we have to rerender the create page, as there were errors
-        return;
-      }else{
-        let newUser = new account(
-          {
-            user_id: uniqid(),
-            supervisor_id: req.body.flag ? uniqid() : supervisor_id,//This ideally checks if the person being created is a supervisor. If they aren't, they should inherit the supervisor_id. Otherwise, it creates a supervisor id for them.
-            username: req.body.username,
-            password: generatehash(req.body.password),
-            fname: req.body.fname,
-            lname: req.body.lname,
-            phone: req.body.phone,
-            email: req.body.email,
-            flag: req.body.flag //Checkbox for supervisor or not
-          });
-        newUser.save(function (err){
-          if (err){return next(err); }
-          res.redirect('To be determined');
+  body('username').isLength({min: 1}).trim().withMessage('Please enter your username').isAlphaNumeric().withMessage('Must be alphanumeric'),
+  body('password').isLength({min: 1}).trim().withMessage('Please enter your password'),
+  body('fname').isLength({min: 1}).trim().withMessage('Please enter your first name').isAlphaNumeric().withMessage('Must be alphanumeric'),
+  body('lname').isLength({min: 1}).trim().withMessage('Please enter your last name').isAlphaNumeric().withMessage('Must be alphanumeric'),
+  body('phone').isLength({min: 12, max: 12}).trim().withMessage('Please enter your phone number (eg. 555-123-4567)'),
+  body('email').isLength({min: 1}).trim().withMessage('Please enter your email (eg. example@domain.com)'),
+  //sanitize
+  sanitize('username').escape(),
+  sanitize('password').escape(),
+  sanitize('fname').escape(),
+  sanitize('lname').escape(),
+  sanitize('phone').escape(),
+  sanitize('email').escape(),
+  (req, res, next) =>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+      //we have to rerender the create page, as there were errors
+      return;
+    }else{
+      let newUser = new account(
+        {
+          user_id: uniqid(),
+          supervisor_id: req.body.flag ? uniqid() : supervisor_id,//This ideally checks if the person being created is a supervisor. If they aren't, they should inherit the supervisor_id. Otherwise, it creates a supervisor id for them.
+          username: req.body.username,
+          password: generatehash(req.body.password),
+          fname: req.body.fname,
+          lname: req.body.lname,
+          phone: req.body.phone,
+          email: req.body.email,
+          flag: req.body.flag //Checkbox for supervisor or not
         });
-      }
+      newUser.save(function (err){
+        if (err){return next(err); }
+        res.redirect('To be determined');
+      });
     }
-]
+  }
+];
+
+exports.edit_user[
+  body('username').isLength({min: 1}).withMessage("Please enter a username").isAlphaNumeric().withMessage("Letters or numbers only"),
+  body('fname').isLength({min: 1}).withMessage("Please enter your first name").isAlphaNumeric().withMessage("Letters or numbers only"),
+  body('lname').isLength({min: 1}).withMessage("Please enter your last name").isAlphaNumeric().withMessage("Letters or numbers only"),
+  body('phone').isLength({min: 12, max: 12}).withMessage("Please enter your phone number (eg. 555-123-4567)"),
+  body('email').isLength({min: 1}).trim().withMessage('Please enter your email (eg. example@domain.com)'),
+  sanitize('username').escape(),
+  sanitize('fname').escape(),
+  sanitize('lname').escape(),
+  sanitize('phone').escape(),
+  sanitize('email').escape(),
+  (req, res, next) =>{
+    let newUser = new account({
+      username: req.body.username,
+      fname: req.body.fname,
+      lname: req.body.lname,
+      phone: req.body.phone,
+      email: req.body.email
+    });
+    account.findByIdAndUpdate(req.params.id, newUser, function(err){
+      if (err){return next(err);}
+      res.redirect('somewhere');
+    });
+  }
+];
