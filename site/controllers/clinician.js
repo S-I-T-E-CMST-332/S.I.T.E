b8dd72234b72eac241cccb73aacba68ff27599dc
Leaderfirestar <EricAWebb2000@yahoo.com>
2020-04-16 11:19:57 -0500

    Changed file-structure to reflect new sitemap

--- site/controllers/clinician.js
+++ site/controllers/clinician.js
@@ -41,12 +41,8 @@ exports.get_card = function(req, res, next){
     res.render('clients/client\ profile/session/sounds/flashcard/flashcard');//This is the controller to load a new flashcard
 }
 
-exports.get_progress = function(req, res, next){
-    res.render('clients/client\ profile/progress/progress');
-}
-
 exports.get_progress_overview = function(req, res, next){
-    res.render('clients/client\ profile/progress/progress\ overview/overview');
+    res.render('clients/client\ profile/progress/overview');
 }
 
 exports.get_progress_sessions = function(req, res, next){
