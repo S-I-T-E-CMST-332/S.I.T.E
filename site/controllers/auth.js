7c8aeb5a3d181d5517264d64d75e00279c6458c9
Leaderfirestar <EricAWebb2000@yahoo.com>
2020-04-23 13:21:56 -0500

    Login function works

--- site/controllers/auth.js
+++ site/controllers/auth.js
@@ -5,7 +5,7 @@ let bcrypt = require('bcrypt');
 let saltRounds = 10;
 let salt = bcrypt.genSaltSync(saltRounds);
 
-exports.login = function(req, res, next) { 
+exports.login = function(req, res, next) {
     check('username').isLength({min: 1}).withMessage('Please enter your username').isAlphanumeric().withMessage('Must be alphanumeric');
     check('password').isLength({min: 1}).withMessage('Please enter your password');
     check('username').trim().escape(),
@@ -14,19 +14,18 @@ exports.login = function(req, res, next) {
       .exec(function(err, user){
         if(err){return next(err);}
         if(user == null){res.render('index', {error: "User does not exist"});}
-        if(bcrypt.compare(req.body.password, user.password)){
-          if(user.flag === true){
-            req.session.user = user.username;
+        if(bcrypt.compare(req.body.password, user[0].password)){
+          if(user[0].flag === true){
+            req.session.id = user[0].user_id;
             req.session.flag = true;
             res.redirect('/clinicians');
           }else{
-            req.session.user = user.username;
+            req.session.user = user[0].user_id;
             req.session.flag = false;
             res.redirect('/clients');
           }
         }else{
           res.render('index', {error:'password is incorrect'});
-          console.log(user.password);
         }
       });
 }
