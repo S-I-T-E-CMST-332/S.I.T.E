fdd24a97d33f1edb53f9a42d9396cff5bf627a84
Leaderfirestar <EricAWebb2000@yahoo.com>
2020-04-24 10:31:30 -0500

    Reverting change

--- site/models/client.js
+++ site/models/client.js
@@ -4,7 +4,7 @@ let Schema = mongoose.Schema;
 
 let ClientSchema = new Schema({
     client_id: {type: String, required: true},
-    user_id: {type: mongoose.Types.ObjectId, ref: 'users', required: true},
+    user_id: {type: Schema.Types.ObjectId, ref: 'users', required: true},
     dob: {type: Date, required: true},
     fname: {type: String, required: true},
     lname: {type: String, required: true}
