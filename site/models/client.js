aff97827a567040e635b9b7657c6fe97c3601643
Leaderfirestar <EricAWebb2000@yahoo.com>
2020-04-24 10:39:17 -0500

    Changed datatype in model

--- site/models/client.js
+++ site/models/client.js
@@ -4,7 +4,7 @@ let Schema = mongoose.Schema;
 
 let ClientSchema = new Schema({
     client_id: {type: String, required: true},
-    user_id: {type: Schema.Types.ObjectId, ref: 'users', required: true},
+    user_id: {type: String, required: true},//This should be equal to a user's id that we're passing as a session variable
     dob: {type: Date, required: true},
     fname: {type: String, required: true},
     lname: {type: String, required: true}
