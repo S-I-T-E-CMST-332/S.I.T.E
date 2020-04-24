15ad672eb9ebd2b8d8e3cba105f934b1fa7243e8
Leaderfirestar <EricAWebb2000@yahoo.com>
2020-04-24 10:29:24 -0500

    trying mongoose.Types.ObjectId

--- site/models/client.js
+++ site/models/client.js
@@ -3,8 +3,8 @@ let mongoose = require("mongoose");
 let Schema = mongoose.Schema;
 
 let ClientSchema = new Schema({
-    client_id: {type: Number, required: true},
-    user_id: {type: Schema.Types.ObjectId, ref: 'users', required: true},
+    client_id: {type: String, required: true},
+    user_id: {type: mongoose.Types.ObjectId, ref: 'users', required: true},
     dob: {type: Date, required: true},
     fname: {type: String, required: true},
     lname: {type: String, required: true}
