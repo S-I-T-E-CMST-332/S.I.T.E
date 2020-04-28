445b3ad7cb024db964f708fa0bfd803e18552fc5
Leaderfirestar <EricAWebb2000@yahoo.com>
2020-04-28 08:36:08 -0500

    Added scores to match erd

--- site/models/letter.js
+++ site/models/letter.js
@@ -5,7 +5,10 @@ let Schema = mongoose.Schema;
 let LetterSchema = new Schema(
     {
         letter_id: {type: Number, required: true},
-        session_id: {type: Schema.Types.ObjectId, ref: 'sessions', required: true}
+        session_id: {type: Schema.Types.ObjectId, ref: 'sessions', required: true},
+        correct: {type: Number, required: false},
+        incorrect: {type: Number, required: false},
+        kinda: {type: Number, required: false}
     }
 );
 
--- site/models/session.js
+++ site/models/session.js
@@ -5,7 +5,10 @@ let Schema = mongoose.Schema;
 let SessionSchema = new Schema({
     session_id: {type: Number, required: true},
     client_id: {type: Schema.Types.ObjectId, ref: 'client', required: true},
-    date: {type: Date, required: true}
+    date: {type: Date, required: true},
+    correct: {type: Number, required: false},
+    incorrect: {type: Number, required: false},
+    kinda: {type: Number, required: false}
 });
 
 SessionSchema
