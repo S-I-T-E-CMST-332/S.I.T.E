fd7a40444af16b41918cecc79de5f57ad7f71b5f
Leaderfirestar <EricAWebb2000@yahoo.com>
2020-05-04 22:16:30 -0500

    Added a session creator

--- site/controllers/client.js
+++ site/controllers/client.js
@@ -2,6 +2,8 @@ const session = require('../models/session');
 const letter = require('../models/letter');
 const form = require('../models/form');
 
+let uniqid = require('uniqid');
+
 exports.correct = function(req, res, next){
     let currentSess = session.findById(req.session.session_id);
     let currentLetter = session.findById(req.session.letter_id);
@@ -72,4 +74,20 @@ exports.kindof = function(req, res, next){
     session.findByIdAndUpdate(req.session.session_id, Session, function(err){
         if(err){return next(err);}
     });
+}
+
+exports.start_session = function(req, res){
+    let Session = new session({
+        session_id: uniqid(),
+        client_id: req.params.client_id,
+        date: new Date(),//This needs to be momented
+        correct: 0,
+        incorrect: 0,
+        kinda: 0
+    });
+    Session.save(function(err){
+        if(err){return next(err);}
+    });
+    req.session.session_id = Session.session_id;
+    next();
 }
