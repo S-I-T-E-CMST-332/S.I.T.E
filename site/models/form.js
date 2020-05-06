c763f83065938a6dc83d761087c5ebec7927352a
Leaderfirestar <EricAWebb2000@yahoo.com>
2020-05-06 10:36:38 -0500

    Revise models

--- site/models/form.js
+++ site/models/form.js
@@ -5,7 +5,7 @@ let Schema = mongoose.Schema;
 let FormSchema = new Schema(
     {
         form_id: {type: Number, required: true},
-        letter_id: {type: Schema.Types.ObjectId, ref: 'letter', required: true},
+        letter_id: {type: String, required: true},
         correct: {type: Number, required: false},
         incorrect: {type: Number, required: false},
         kinda: {type: Number, required: false}
--- site/models/letter.js
+++ site/models/letter.js
@@ -5,7 +5,7 @@ let Schema = mongoose.Schema;
 let LetterSchema = new Schema(
     {
         letter_id: {type: Number, required: true},
-        session_id: {type: Schema.Types.ObjectId, ref: 'sessions', required: true},
+        session_id: {type: String, required: false},
         correct: {type: Number, required: false},
         incorrect: {type: Number, required: false},
         kinda: {type: Number, required: false}
