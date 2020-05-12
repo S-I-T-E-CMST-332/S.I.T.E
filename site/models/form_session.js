64c93048eee7399b42b472e86f7faac50beadf26
Leaderfirestar <EricAWebb2000@yahoo.com>
2020-05-12 17:07:24 -0500

    Added to seperate scoring

---
+++ site/models/form_session.js
@@ -0,0 +1,15 @@
+let mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let SessFormSchema = new Schema(
+    {
+        form_id: {type: String, required: true},
+        session_id: {type: String, required: true},
+        correct: {type: Number, required: false},
+        incorrect: {type: Number, required: false},
+        kinda: {type: Number, required: false},
+    }
+);
+
+module.exports = mongoose.model('sess_forms', SessFormSchema);
