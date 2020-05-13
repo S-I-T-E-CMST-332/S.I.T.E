6fa6769881a51899fd9a5d7376b228a8405699c5
Leaderfirestar <EricAWebb2000@yahoo.com>
2020-05-13 14:18:35 -0500

    Created listeners for server response

--- site/public/javascripts/ajax.js
+++ site/public/javascripts/ajax.js
@@ -1,4 +1,5 @@
 let xhttp = new XMLHttpRequest();
+let image = document.getElementById("image");
 let correct = document.getElementById("correct");
 let incorrect = document.getElementById("incorrect");
 let kindOf = document.getElementById("kindOf");
@@ -6,15 +7,27 @@ let kindOf = document.getElementById("kindOf");
 correct.addEventListener('click', function(e){
     e.preventDefault();
     xhttp.open('POST', '/correct');
+    xhttp.onload = function(){
+        let newImage = this.responseText;
+        image.src = newImage;
+    }
     xhttp.send();
 });
 incorrect.addEventListener('click', function(e){
     e.preventDefault();
     xhttp.open('POST', '/incorrect');
+    xhttp.onload = function(){
+        let newImage = this.responseText;
+        image.src = newImage;
+    }
     xhttp.send();
 });
 kindOf.addEventListener('click', function(e){
     e.preventDefault();
     xhttp.open('POST', '/kindof');
+    xhttp.onload = function(){
+        let newImage = this.responseText;
+        image.src = newImage;
+    }
     xhttp.send();
 });
