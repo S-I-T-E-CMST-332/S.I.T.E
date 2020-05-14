4e5c785a7bddea042f9a44298e4bfcdb8225a51e
Leaderfirestar <EricAWebb2000@yahoo.com>
2020-05-14 10:06:33 -0500

    Added in virtual I accidentally removed

--- site/models/letter.js
+++ site/models/letter.js
@@ -8,4 +8,10 @@ let LetterSchema = new Schema(
     }
 );
 
+LetterSchema
+.virtual('name')
+.get(function(){
+  return this.letter_id;
+});
+
 module.exports = mongoose.model('letter', LetterSchema);
