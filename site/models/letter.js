a9567536dce667d15035d4897ba5a68e3643f118
Leaderfirestar <EricAWebb2000@yahoo.com>
2020-05-13 20:38:02 -0500

    Moved scoring to letter_session, using letter as more of reference

--- site/models/letter.js
+++ site/models/letter.js
@@ -5,23 +5,7 @@ let Schema = mongoose.Schema;
 let LetterSchema = new Schema(
     {
         letter_id: {type: String, required: true},
-        session_id: {type: String, required: false},
-        correct: {type: Number, required: false},
-        incorrect: {type: Number, required: false},
-        kinda: {type: Number, required: false}
     }
 );
-
-LetterSchema
-.virtual('name')
-.get(function(){
-  return this.letter_id;
-});
-
-LetterSchema
-.virtual('total')
-.get(function(){
-  return this.correct + this.incorrect + this.kinda;
-});
 
 module.exports = mongoose.model('letter', LetterSchema);
---
+++ site/models/letter_session.js
@@ -0,0 +1,27 @@
+let mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let SessLetterSchema = new Schema(
+    {
+        letter_id: {type: String, required: true},
+        session_id: {type: String, required: true},
+        correct: {type: Number, required: false},
+        incorrect: {type: Number, required: false},
+        kinda: {type: Number, required: false}
+    }
+);
+
+SessLetterSchema
+.virtual('name')
+.get(function(){
+  return this.letter_id;
+});
+
+SessLetterSchema
+.virtual('total')
+.get(function(){
+  return this.correct + this.incorrect + this.kinda;
+});
+
+module.exports = mongoose.model('sess_letter', SessLetterSchema);
