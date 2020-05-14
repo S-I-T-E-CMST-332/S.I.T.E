a6878b1eae15b17751ff4401c32b1de0f7283b9c
Leaderfirestar <EricAWebb2000@yahoo.com>
2020-05-13 23:01:09 -0500

    Made great strides in debugging

--- site/controllers/client.js
+++ site/controllers/client.js
@@ -3,61 +3,88 @@ const letter = require('../models/letter');
 const form = require('../models/form');
 const flashcard = require('../models/flashcard');
 const formsession = require('../models/form_session');
-let client = require('../models/client');
+const client = require('../models/client');
+const lettersession = require('../models/letter_session');
 
 let uniqid = require('uniqid');
+let async = require('async');
+let moment = require('moment');
 
 exports.correct = function(req, res, next){
-    let currentSess = session.findById(req.session.session_id);
-    let currentLetter = letter.find({"letter_id": req.session.letter_id});
-    let currentFormSess = formsession.find({"form_id": currentLetter[0].form_id});
-    let Letter = new letter({
-        letter_id: currentLetter[0].letter_id,
-        correct: currentLetter[0].correct++
-    });
-    let Form = new formsession({
-        form_id: currentFormSess[0].form_id,
-        correct: currentFormSess[0].correct++
-    });
-    let Session = new session({
-        session_id: currentSess.session_id,
-        correct: currentSess.correct++
-    });
-    letter.findByIdAndUpdate(currentLetter[0]._id, Letter, function(err){
-        if(err){return next(err);}
-    }),
-    form.findByIdAndUpdate(currentFormSess[0]._id, Form, function(err){
-        if(err){return next(err);}
-    }),
-    session.findByIdAndUpdate(req.session.session_id, Session, function(err){
-        if(err){return next(err);}
-    });
-    next();
+    async.parallel({
+        session: function(callback){
+            session.find({"session_id": req.session.session_id}).exec(callback)
+        },
+        lettersession: function(callback){
+            lettersession.find({"letter_id": req.session.letter_id, "session_id": req.session.session_id}).exec(callback)
+        },
+        formsession: function(callback){
+            formsession.find({"form_id": req.session.form_id, "session_id": req.session.session_id}).exec(callback)
+        },
+    }, function(err, results){
+        let Session = new session({
+            _id: results.session[0]._id,
+            session_id: req.session.session_id,
+            client_id: req.session.client_id,
+            correct: results.session[0].correct++
+        });
+        session.findByIdAndUpdate(results.session[0]._id, Session, {new: true}, function(err){
+            if(err){return next(err);}
+            console.log('updates session');
+        });
+        let Letter = new lettersession({
+            _id: results.lettersession[0]._id,
+            letter_id: results.lettersession[0].letter_id,
+            session_id: req.session.session_id,
+            correct: results.lettersession[0].correct++
+        });
+        lettersession.findByIdAndUpdate(results.lettersession[0]._id, Letter, {new: true}, function(err){
+            if(err){return next(err);}
+            console.log('updates letter');
+        });
+        let Form = new formsession({
+            _id: results.formsession[0]._id,
+            form_id: req.session.form_id,
+            session_id: req.session.session_id,
+            correct: results.formsession[0].correct++
+        });
+        formsession.findByIdAndUpdate(results.formsession[0]._id, Form, {new: true}, function(err){
+            if(err){return next(err);}
+            console.log('updates formsession');
+        });
+    },function(){
+        console.log('calls next')
+        next()
+    }
+    );
 }
 
 exports.incorrect = function(req, res, next){
-    let currentSess = session.findById(req.session.session_id);
-    let currentLetter = letter.find({"session_id": req.session.letter_id});
-    let currentFormSess = formsession.find({"letter_id": currentLetter[0].letter_id});
-    let Letter = new letter({
-        letter_id: currentLetter[0].letter_id,
-        incorrect: currentLetter[0].incorrect++
-    });
-    let Form = new formsession({
-        form_id: currentFormSess[0].form_id,
-        incorrect: currentFormSess[0].incorrect++
-    });
-    let Session = new session({
-        session_id: currentSess.session_id,
-        incorrect: currentSess.incorrect++
+    let currentSess = session.find({"session_id": req.session.session_id});
+    let currentLetterSess = lettersession.find({"letter_id": req.session.letter_id});
+    let currentFormSess = formsession.find({"form_id": req.session.form_id});
+    let Letter = new lettersession({
+        letter_id: currentLetterSess[0].letter_id,
+        session_id: req.session.session_id,
+        incorrect: currentLetterSess[0].incorrect++
     });
     letter.findByIdAndUpdate(currentLetter[0]._id, Letter, function(err){
         if(err){return next(err);}
-    }),
+    });
+    let Form = new formsession({
+        form_id: req.params.sound_id,
+        session_id: req.session.session_id,
+        incorrect: currentFormSess[0].incorrect++
+    });
     form.findByIdAndUpdate(currentFormSess[0]._id, Form, function(err){
         if(err){return next(err);}
-    }),
-    session.findByIdAndUpdate(req.session.session_id, Session, function(err){
+    });
+    let Session = new session({
+        session_id: req.session.session_id,
+        client_id: req.session.client_id,
+        incorrect: currentSess[0].incorrect++
+    });
+    session.findByIdAndUpdate(currentSess[0]._id, Session, function(err){
         if(err){return next(err);}
     });
     next();
@@ -66,17 +93,19 @@ exports.incorrect = function(req, res, next){
 exports.kindof = function(req, res, next){
     let currentSess = session.findById(req.session.session_id);
     let currentLetter = letter.find({"letter_id": req.session.letter_id});
-    let currentFormSess = form.find({"form_id": currentForm[0].form_id});
+    let currentFormSess = form.find({"form_id": req.session.form_id});
     let Letter = new letter({
         letter_id: currentLetter[0].letter_id,
         kinda: currentLetter[0].kinda++
     });
     let Form = new formsession({
         form_id: currentFormSess[0].form_id,
+        session_id: req.session.session_id,
         kinda: currentFormSess[0].kinda++
     });
     let Session = new session({
         session_id: currentSess.session_id,
+        client_id: req.session.client_id,
         kinda: currentSess.kinda++
     });
     letter.findByIdAndUpdate(currentLetter[0]._id, Letter, function(err){
@@ -94,10 +123,11 @@ exports.kindof = function(req, res, next){
 exports.start_session = function(req, res, next){
     client.findById(req.params.client_id).exec(function(err, client){
         if(err){return next(err);}
+        let day = new Date()
         let Session = new session({
             session_id: uniqid(),
             client_id: client.client_id,
-            date: new Date(),//This needs to be momented
+            date: moment(day).utc(),
             correct: 0,
             incorrect: 0,
             kinda: 0
@@ -121,8 +151,24 @@ exports.create_form_session = function(req, res, next){
     });
     FormSession.save(function(err){
         if(err){return next(err);}
-        next();
     });
+    req.session.letter_id = 'r';
+    req.session.form_id = req.params.sound_id;
+    next();
+}
+
+exports.create_letter_session = function(req, res, next){
+    let LetterSession = new lettersession({
+        letter_id: req.session.letter_id,
+        session_id: req.session.session_id,
+        correct: 0,
+        incorrect: 0,
+        kinda: 0
+    });
+    LetterSession.save(function(err){
+        if(err){return next(err);}
+    });
+    next();
 }
 
 exports.get_card = function(req, res, next){
@@ -134,9 +180,17 @@ exports.get_card = function(req, res, next){
 }
 
 exports.send_card = function(req, res, next){
-    flashcard.find({'form_id': req.params.sound_id})
+    flashcard.find({'form_id': req.session.form_id})
         .exec(function(err, cards){
             if(err){return next(err);}
-            return({newImage: cards[Math.floor(Math.random()*cards.length)].link});
-        });//Try a return
+            res.send(cards[Math.floor(Math.random()*cards.length)].link);
+        });
+}
+
+exports.get_ajax = function(req, res){
+    res.render('ajax');
+}
+
+exports.send_stuff = function(req, res){
+    res.send('/images/flashcards/capture.png');
 }
